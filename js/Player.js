1840e4f2874fd575bd89208960e09eec10bfe6e8
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2020-07-17 17:56:32 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,51 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = 0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+    getPlayerRank() {
+      if(this.distance === 3500){
+        var carPositionRef = database.ref('carPosition');
+        carPositionRef.on("value",(data)=>{
+           this.rank = data.val(); 
+        }) 
+      }
+  }
+  static  updatePlayerRank(rnk){
+        database.ref('/').update({
+          carPosition : rnk 
+        });
+      
+     }
+}
